317535a869f5ce19c9022dd9f8d3a2d144c94fe8
PyRo1121 <olen@latham.cloud>
2025-02-17 19:21:46 -0600

    updates

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+# Load environment variables from .env file in development
+if os.path.exists('.env'):
+    load_dotenv()
 
 # Twitch Configuration
-TWITCH_TOKEN = os.getenv('TWITCH_TOKEN')
+TWITCH_TOKEN = os.environ.get('TWITCH_TOKEN')
 TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
 TWITCH_CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
 TWITCH_ACCESS_TOKEN = os.getenv('TWITCH_ACCESS_TOKEN')
@@ -17,7 +18,7 @@ TWITCH_MONITOR_CHANNEL = os.getenv('TWITCH_MONITOR_CHANNEL')
 # Discord Configuration
 DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID', 0))
 DISCORD_CLIENT_ID = int(os.getenv('DISCORD_CLIENT_ID', 0))
-DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 DISCORD_GUILD_ID = int(os.getenv('DISCORD_GUILD_ID', '0'))
 
 # Simple Bot Invite URL
